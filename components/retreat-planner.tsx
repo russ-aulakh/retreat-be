f45385af016ffff7d48815d58735a8d4c6d54041
russ.aulakh <russ.aulakh@gmail.com>
2025-03-18 22:16:36 +0530

    lol

--- components/retreat-planner.tsx
+++ components/retreat-planner.tsx
@@ -302,7 +302,7 @@ export default function RetreatPlanner() {
 
     try {
       // Example fetch
-      const response = await fetch("https://re.flyfast.io/analyzeCosts", {
+      const response = await fetch("https://ret.flyfast.io/analyzeCosts", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestData),
